7416b3f29ac4eece83511d1abe0acc4d217f97bd
Aditibhorkar <127037400+Aditibhorkar@users.noreply.github.com>
2024-06-09 11:11:05 +0530

    Add files via upload

---
+++ FillSquare.java
@@ -0,0 +1,16 @@
+package PatternProgram;
+
+public class FillSquare {
+
+public static void main(String[] args) {
+		
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
